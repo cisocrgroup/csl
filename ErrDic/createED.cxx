23f69a7301a13105d442ec82fbe8f12be8b3e0cf
reffle <reffle@f2d9b42b-cd0b-0410-95c1-856273910ab5>
2007-09-18 14:54:47 +0000

    A    trunk/ErrDic/createED.cxx
    git-svn-id: svn://svn.cis.uni-muenchen.de/csl/trunk@210 f2d9b42b-cd0b-0410-95c1-856273910ab5

---
+++ ErrDic/createED.cxx
@@ -0,0 +1,26 @@
+#include<iostream>
+
+#include "./ErrDicConstructor.h"
+
+int main( int argc, char** argv ) {
+
+    if( argc != 4 ) {
+	std::cerr<<"Use like: "<<argv[0]<<" <pos._dic> <filterDic> <patternFile>"<<std::endl;
+	exit(1);
+    }
+
+
+    setlocale(LC_CTYPE, "de_DE.UTF-8");  /*Setzt das Default Encoding für das Programm */
+
+    csl::MinDic< int > dic( argv[1]);
+    csl::MinDic< int > filterDic( argv[2]);
+
+
+    csl::ErrDic ed;
+    csl::ErrDicConstructor edc;
+    edc.constructErrDic( dic, filterDic, argv[3], ed );
+    ed.writeToFile( "./test.errdic" );
+    
+    
+}
+
