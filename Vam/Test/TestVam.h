021aff29acb34d32c0e78e818164f672fb817f2a
reffle <reffle@f2d9b42b-cd0b-0410-95c1-856273910ab5>
2008-02-13 09:19:29 +0000

    kleine Zugfahrt-Fortschritte bei VamTest
    git-svn-id: svn://svn.cis.uni-muenchen.de/csl/trunk@257 f2d9b42b-cd0b-0410-95c1-856273910ab5

--- Vam/Test/TestVam.h
+++ Vam/Test/TestVam.h
@@ -1,8 +1,8 @@
-#ifndef CSL_TESTMINDIC_H
-#define CSL_TESTMINDIC_H CSL_TESTMINDIC_H
+#ifndef CSL_TEST_VAM_H
+#define CSL_TEST_VAM_H CSL_TEST_VAM_H
 
 #include "../../Global.h"
-#include "../MinDic.h"
+#include "../Vam.h"
 
 #include <cppunit/extensions/HelperMacros.h>
 
@@ -14,9 +14,6 @@ namespace csl {
 	CPPUNIT_TEST( testBasics );
 	CPPUNIT_TEST_SUITE_END();
     public:
-	TestMinDic();
-
-	void run();
 
 	void testBasics();
 // 	void testConstructionDetails();
@@ -24,49 +21,20 @@ namespace csl {
 
 
     private:
-	csl::Alphabet alph_;
-	char* lexFile_;
-	csl::MinDic<> mdic_;
 	
     };
 
     /**
      * test the basic methods for reading access like getRoot, walk, isFinal etc.
      */
-    void TestMinDic::testBasics() {
-
+    void TestVam::testBasics() {
+	CPPUNIT_ASSERT( 1 );
+	CPPUNIT_ASSERT( 0 );
 
     }
 
-//     void TestMinDic::lookupAllKeys() {
-// 	std::ifstream fileHandle( lexFile_ );
-// 	if ( !fileHandle.good() ) {
-// 	    throw exceptions::badFileHandle( "Couldn't open file '" +
-// 					     std::string( lexFile_ ) +
-// 					     "' for reading." );
-// 	}
-// 	uchar line[Global::lengthOfLongStr];
-// 	uchar* valueString = 0;
-	
-// 	bool testOk = true;
-// 	size_t nrOfLines = 0;
-// 	int value;
-// 	while ( fileHandle.getline( ( char* ) line, Global::lengthOfLongStr ) )  {
-// 	    uchar* c = ( uchar* )strchr( ( char* )line, Global::keyValueDelimiter );
-	    
-// 	    if( c ) {
-// 		*c = 0;
-// 		valueString = ( c + 1 );
-// 	    }
-// 	    uint_t pos = mdic_.walkStr( mdic_.getRoot(), line );
-// 	    assert( mdic_.getAnnotation( line, &value ) );
-// 	    testOk = testOk && pos && mdic_.isFinal( pos ) && ( atoi( (char*)valueString ) == value );
-	    
-// 	    ++nrOfLines;
-// 	}
-// 	assert( nrOfLines == mdic_.getNrOfKeys() );
-// 	assert( testOk );
-//     }
+
+
 } // namespace csl
 
 
--- Vam/Test/testVam.cxx
+++ Vam/Test/testVam.cxx
@@ -3,18 +3,21 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
-#include "./TestMinDic.h"
+#include "./TestVam.h"
 
 
 int main() {
-    // Get the top level suite from the registry
-    CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+//     // Get the top level suite from the registry
+//     CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
 
+    CppUnit::TestSuite suite;
+
+    suite.addTest( new CppUnit::TestCaller< VamTest >( "testBasics",  ) );
 
     // Adds the test to the list of test to run
     CppUnit::TextUi::TestRunner runner;
     runner.addTest( suite );
-    runner.addTest(  );
+    runner.addTest( csl::TestVam );
     
     // Change the default outputter to a compiler error format outputter
     runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(),
@@ -23,9 +26,8 @@ int main() {
     bool wasSucessful = runner.run();
 
     // Return error code 1 if the one of test failed.
-    return wasSucessful ? 0 : 1;    csl::TestMinDic testMD;
+    return wasSucessful ? 0 : 1;    
 
-    testMD.run();
 
     return 0;
 }
